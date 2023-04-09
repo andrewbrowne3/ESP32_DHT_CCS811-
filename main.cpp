11dbf4d78335b44b291c2b705ea5898452e7bc29
Andrew Browne <65584938+andrewbrowne3@users.noreply.github.com>
2023-04-08 23:24:27 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,23 +1,30 @@
 #include <Arduino.h>
 #include <Wire.h>
 #include <LiquidCrystal_I2C.h>
-
+#include <Adafruit_GFX.h>
 #include "Adafruit_CCS811.h"
 #include <SPI.h>
+#include <DHT.h> // Include the DHT library
 
 // Define the LED pin (usually GPIO 2 for the onboard LED on ESP32)
 const int ledPin = 2;
+float temp;
 
 // Define the capacitive touch sensor pin
 const int touchSensorPin = 4; // Replace '4' with the GPIO pin you connected the touch sensor to
 
+// Define the DHT pin and type
+const int dhtPin = 5; // Replace '5' with the GPIO pin you connected the DHT11 to
+#define DHTTYPE DHT11 // Define the DHT type as DHT11
+
 // Store the current state of the LED
 bool ledState = false;
 
-// Store the last state of the tobuch sensor
+// Store the last state of the touch sensor
 int lastTouchSensorState = LOW;
 int x = 0;
 
+DHT dht(dhtPin, DHTTYPE); // Initialize the DHT object
 
 Adafruit_CCS811 ccs;
 // Initialize the I2C LCD (adjust the address if needed, usually 0x27 or 0x3F)
@@ -35,7 +42,9 @@ void setup() {
 
   // Initialize the LCD
   lcd.init();
-   
+
+  // Initialize the DHT sensor
+  dht.begin();
 
   if (!ccs.begin()) {
     Serial.println("Failed to start CCS811 sensor! Please check your wiring.");
@@ -55,8 +64,6 @@ void loop() {
   // Read the capacitive touch sensor value
   int touchSensorValue = digitalRead(touchSensorPin);
 
- 
-
   // Check if the touch sensor state has changed from not touched to touched
   if (lastTouchSensorState == LOW && touchSensorValue == HIGH) {
     // Toggle the LED state
@@ -67,35 +74,78 @@ void loop() {
 
     // Toggle the LCD backlight state
     lcdBacklightState = !lcdBacklightState;
-  
-    if (lcdBacklightState) {
-      // Turn on the LCD backlight
-      lcd.backlight();
+  }
 
-      // Print a message on the first line
-      lcd.setCursor(0, 0);
-      lcd.print("testing...");
-      delay(100);
-      lcd.clear();
-      lcd.setCursor(0, 0);
-        if(ccs.available()){
-    
-      float temp = ccs.calculateTemperature();
-      if(ccs.readData()){
-        lcd.print("eCO2: ");
-        float eCO2 = ccs.geteCO2();
-        lcd.print(eCO2);
+  if (lcdBacklightState) {
+    // Turn on the LCD backlight
+    lcd.backlight();
+
+    float temperature = dht.readTemperature();
+    float humidity = dht.readHumidity();
+
+    // Print humidity values on the first line
+    lcd.setCursor(0, 0);
+    lcd.print("Humidity: ");
+    if (isnan(humidity)) {
+      lcd.print("Err");
+    } else {
+      lcd.print(humidity);
+      lcd.print("%");
+    }
+    delay(2000);
+    lcd.clear();
+
+    // Print temperature values
+    lcd.setCursor(0, 0);
+    lcd.print("Temp: ");
+    if (isnan(temperature)) {
+      lcd.print("Err");
+    } else {
+      lcd.print(temperature);
+      lcd.print((char)223);
+      lcd.print("C");
+    }
+    delay(2000);
+    lcd.clear();
+
+    // Calculate and print PH2O values
+    const float A = 6.11; // hPa
+    const float B = 5310.0; // K
+    const float t0 = 273.0; // K
+    float x = B * (1 / t0 - 1 / (temperature + t0));
+    float PH2Osat = A * exp(x);
+    float PH2O = (humidity * PH2Osat) / 100;
+
+    lcd.setCursor(0, 0);
+    lcd.print("P H2O: ");
+    lcd.print(PH2O);
+    delay(2000);
+    lcd.clear();
+
+    if (ccs.available()) {
+      if (!ccs.readData()) {
+        // Print CO2 values
+        lcd.setCursor(0, 0);
+        lcd.print("CO2: ");
+        lcd.print(ccs.geteCO2());
+        lcd.print("ppm");
+        delay(2000);
+        lcd.clear();
+
+        // Print TVOC values
+        lcd.setCursor(0, 0);
+        lcd.print("TVOC: ");
+        lcd.print(ccs.getTVOC());
+        delay(2000);
+        lcd.clear();
       }
     }
-    } else {
-      // Turn off the LCD backlight
-      lcd.noBacklight();
-
-      // Clear the LCD
-      lcd.clear();
-    }
   } else {
-    Serial.println("not touching button");
+    // Turn off the LCD backlight
+    lcd.noBacklight();
+
+    // Clear the LCD
+    lcd.clear();
   }
 
   // Update the last touch sensor state
@@ -103,4 +153,4 @@ void loop() {
 
   // Add a small delay to debounce the touch sensor
   delay(100);
-} 
+}
