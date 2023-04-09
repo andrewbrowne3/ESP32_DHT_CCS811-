cf2a2979f8395840a10c85faa305ee9022453ef1
Andrew Browne <65584938+andrewbrowne3@users.noreply.github.com>
2023-04-08 22:08:36 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,7 +1,9 @@
-
 #include <Arduino.h>
 #include <Wire.h>
 #include <LiquidCrystal_I2C.h>
+
+#include "Adafruit_CCS811.h"
+#include <SPI.h>
 
 // Define the LED pin (usually GPIO 2 for the onboard LED on ESP32)
 const int ledPin = 2;
@@ -12,11 +14,12 @@ const int touchSensorPin = 4; // Replace '4' with the GPIO pin you connected the
 // Store the current state of the LED
 bool ledState = false;
 
-// Store the last state of the touch sensor
+// Store the last state of the tobuch sensor
 int lastTouchSensorState = LOW;
+int x = 0;
 
 
-
+Adafruit_CCS811 ccs;
 // Initialize the I2C LCD (adjust the address if needed, usually 0x27 or 0x3F)
 LiquidCrystal_I2C lcd(0x27, 16, 2); // Set the LCD address to 0x27 for a 16 chars and 2 line display
 
@@ -32,7 +35,19 @@ void setup() {
 
   // Initialize the LCD
   lcd.init();
+   
+
+  if (!ccs.begin()) {
+    Serial.println("Failed to start CCS811 sensor! Please check your wiring.");
+    while (1);
+  }
+
+  // Calibrate temperature sensor
+  while (!ccs.available());
+  float temp = ccs.calculateTemperature();
+  ccs.setTempOffset(temp - 25.0);
 }
+
 // Store the current state of the LCD backlight
 bool lcdBacklightState = false;
 
@@ -40,9 +55,7 @@ void loop() {
   // Read the capacitive touch sensor value
   int touchSensorValue = digitalRead(touchSensorPin);
 
-  // Print the touch sensor value to the serial monitor
-  Serial.print("Touch Sensor Value: ");
-  Serial.println(touchSensorValue);
+ 
 
   // Check if the touch sensor state has changed from not touched to touched
   if (lastTouchSensorState == LOW && touchSensorValue == HIGH) {
@@ -54,7 +67,7 @@ void loop() {
 
     // Toggle the LCD backlight state
     lcdBacklightState = !lcdBacklightState;
-
+  
     if (lcdBacklightState) {
       // Turn on the LCD backlight
       lcd.backlight();
@@ -62,6 +75,18 @@ void loop() {
       // Print a message on the first line
       lcd.setCursor(0, 0);
       lcd.print("testing...");
+      delay(100);
+      lcd.clear();
+      lcd.setCursor(0, 0);
+        if(ccs.available()){
+    
+      float temp = ccs.calculateTemperature();
+      if(ccs.readData()){
+        lcd.print("eCO2: ");
+        float eCO2 = ccs.geteCO2();
+        lcd.print(eCO2);
+      }
+    }
     } else {
       // Turn off the LCD backlight
       lcd.noBacklight();
@@ -78,4 +103,4 @@ void loop() {
 
   // Add a small delay to debounce the touch sensor
   delay(100);
-}
+} 
